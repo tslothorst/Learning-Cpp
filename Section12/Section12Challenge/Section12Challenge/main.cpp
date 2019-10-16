f0475f10ac5320ad3878b9a017d441f1d6b532b8
tslothorst <tslothorst@gmail.com>
2019-10-16 19:21:38 +0200

    Completed section 12 challenge

--- Section12/Section12Challenge/Section12Challenge/main.cpp
+++ Section12/Section12Challenge/Section12Challenge/main.cpp
@@ -39,8 +39,9 @@
    Result: [ 10 20 30 40 50 20 40 60 80 100 30 60 90 120 150 ]
 */
 
-
 using namespace std;
+int* apply_all(const int *const a1, const size_t s1, const int *const a2, const size_t s2);
+void print(const int* const a1, const size_t s1);
 
 int main() {
 	const size_t array1_size{ 5 };
@@ -52,6 +53,7 @@ int main() {
 	cout << "Array 1: ";
 	print(array1, array1_size);
 
+
 	cout << "Array 2: ";
 	print(array2, array2_size);
 
@@ -62,6 +64,37 @@ int main() {
 	print(results, results_size);
 
 	cout << endl;
+	delete [] results;
 
 	return 0;
+}
+
+int* apply_all(const int* const a1, const size_t s1, const int* const a2, const size_t s2)
+{
+	// Allocate a new array on the heap with the size of array1 * array2
+	int* result = new int[s1 * s2];
+	// Below is to increment through the memory reserved on the heap through all iterations
+	int memsize{};
+	// Iterate through array 2
+	for (size_t i = 0; i < s2; i++)
+	{
+		// Iterate through array 1 and multiply the value with the value out of array 2 from the first loop
+		for (size_t j = 0; j < s1; j++) 
+		{
+			// Dereference pointer result with off-set of int memsize and store the result of the multiplication
+			*(result + memsize) = a2[i] * a1[j];
+			memsize++;
+		}
+	}
+	// Return the memory address of the first value of the third array
+	return result;
+}
+
+void print(const int* const a1, const size_t s1) 
+{
+	for (size_t i = 0; i < s1; i++)
+	{
+		cout << a1[i] << " ";
+	}
+	cout << endl;
 }
