f0388d1ecf3ad7fd6054411676cfa05e8147963d
tslothorst <tslothorst@gmail.com>
2019-11-04 16:07:12 +0100

    Implemented trust account cpp file

--- Section15/Section15Challenge/Section15Challenge/Trust_Account.cpp
+++ Section15/Section15Challenge/Section15Challenge/Trust_Account.cpp
@@ -1,1 +1,28 @@
 #include "Trust_Account.h"
+
+Trust_Account::Trust_Account(std::string name, double balance, double int_rate) : Savings_Account{ name, balance }, int_rate{ int_rate }
+{
+}
+
+bool Trust_Account::deposit(double amount)
+{
+	if (amount >= 5000.00)
+		amount += 50.00;
+	return Savings_Account::deposit(amount);
+}
+
+bool Trust_Account::withdraw(double amount)
+{
+	int NumberWithdraws{};
+	if (NumberWithdraws < 4) 
+	{
+		NumberWithdraws++;
+		return Account::withdraw(amount);
+	}
+	return false;
+}
+
+std::ostream& operator<<(std::ostream& os, const Trust_Account& account) {
+	os << "[Savings_Account: " << account.name << ": " << account.balance << ", " << account.int_rate << "%]";
+	return os;
+}
--- Section15/Section15Challenge/Section15Challenge/Trust_Account.h
+++ Section15/Section15Challenge/Section15Challenge/Trust_Account.h
@@ -4,7 +4,7 @@
 class Trust_Account :
 	public Savings_Account
 {
-	friend std::ostream& operator<<(std::ostream& os, const Savings_Account& account);
+	friend std::ostream& operator<<(std::ostream& os, const Trust_Account& account);
 private:
 	static constexpr const char* def_name = "Unnamed Trust Account";
 	static constexpr double def_balance = 0.0;
@@ -13,6 +13,7 @@ protected:
 	double int_rate;
 public:
 	Trust_Account(std::string name = def_name, double balance = def_balance, double int_rate = def_int_rate);
+	bool deposit(double amount);
 	bool withdraw(double amount);	
 };
 
