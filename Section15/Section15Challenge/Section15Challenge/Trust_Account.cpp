e261b1d38ea89e302ca131187cf8c07612603d31
tslothorst <tslothorst@gmail.com>
2019-11-04 16:22:08 +0100

    Still some logic errors in trust account

--- Section15/Section15Challenge/Section15Challenge/Trust_Account.cpp
+++ Section15/Section15Challenge/Section15Challenge/Trust_Account.cpp
@@ -13,13 +13,14 @@ bool Trust_Account::deposit(double amount)
 
 bool Trust_Account::withdraw(double amount)
 {
-	int NumberWithdraws{};
+	int NumberWithdraws{0};
 	if (NumberWithdraws < 4) 
 	{
-		NumberWithdraws++;
+		++NumberWithdraws;
 		return Account::withdraw(amount);
 	}
-	return false;
+	if(NumberWithdraws > 3)
+		return false;
 }
 
 std::ostream& operator<<(std::ostream& os, const Trust_Account& account) {
