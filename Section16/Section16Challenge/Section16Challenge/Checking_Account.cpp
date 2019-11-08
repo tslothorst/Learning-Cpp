f0a7cc8a655d954fc1a5a794ffd6610f5715f823
tslothorst <tslothorst@gmail.com>
2019-11-08 09:53:24 +0100

    Implemented virtual functions for checking account class

--- Section16/Section16Challenge/Section16Challenge/Checking_Account.cpp
+++ Section16/Section16Challenge/Section16Challenge/Checking_Account.cpp
@@ -4,11 +4,34 @@ Checking_Account::Checking_Account(std::string name, double balance): Account{ n
 {
 }
 
+Checking_Account::~Checking_Account()
+{
+}
+
 bool Checking_Account::withdraw(double amount)
 {
-	if (balance - amount >= 0)
+	if (balance - amount >= 0) {
 		balance -= withdraw_fee;
-	return Account::withdraw(amount);	
+		balance -= amount;
+		return true;
+	}
+	else
+		return false;
+}
+
+bool Checking_Account::deposit(double amount)
+{
+	if (amount < 0)
+		return false;
+	else {
+		balance += amount;
+		return true;
+	}
+}
+
+double Checking_Account::get_balance() const
+{
+	return balance;
 }
 
 std::ostream& operator<<(std::ostream& os, const Checking_Account& account) 
--- Section16/Section16Challenge/Section16Challenge/Checking_Account.h
+++ Section16/Section16Challenge/Section16Challenge/Checking_Account.h
@@ -12,6 +12,9 @@ private:
 	double withdraw_fee;
 public:
 	Checking_Account(std::string name = def_name, double balance = def_balance);
-	bool withdraw(double amount);
+	virtual ~Checking_Account();
+	virtual bool withdraw(double amount) override;
+	virtual bool deposit(double amount) override;
+	virtual double get_balance() const override;
 };
 
