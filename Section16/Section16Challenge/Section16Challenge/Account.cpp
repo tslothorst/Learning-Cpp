7ae99338bb27a8edf6989b0dc0cc879cb043bbcf
tslothorst <tslothorst@gmail.com>
2019-11-09 20:09:00 +0100

    Changed account class to inherit I_Printable

--- Section16/Section16Challenge/Section16Challenge/Account.cpp
+++ Section16/Section16Challenge/Section16Challenge/Account.cpp
@@ -31,8 +31,3 @@ double Account::get_balance() const {
 	return balance;
 }
 
-std::ostream& operator<<(std::ostream& os, const Account& account) {
-	os << "[Account: " << account.name << ": " << account.balance << "]";
-	return os;
-}
-
--- Section16/Section16Challenge/Section16Challenge/Account.h
+++ Section16/Section16Challenge/Section16Challenge/Account.h
@@ -1,10 +1,10 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include "I_Printable.h"
 
-class Account
+class Account : public I_Printable
 {
-	friend std::ostream& operator<<(std::ostream& os, const Account& account);
 private:
 	static constexpr const char* def_name = "Unnamed Account";
 	static constexpr double def_balance = 0.0;
@@ -14,6 +14,7 @@ protected:
 public:
 	Account(std::string name = def_name, double balance = def_balance);
 	virtual ~Account();
+	virtual void print(std::ostream& os) const override = 0;
 	//Account(std::string name = "Unamed Account", double balance = 0.0);
 	virtual bool deposit(double amount) = 0;
 	virtual bool withdraw(double amount) = 0;
