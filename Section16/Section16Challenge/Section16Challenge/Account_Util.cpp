0d6dc468b2e9327c691b58fcf1e87133a7d049b7
tslothorst <tslothorst@gmail.com>
2019-11-11 11:59:38 +0100

    Fixed Account_Util not dereferencing pointers

--- Section16/Section16Challenge/Section16Challenge/Account_Util.cpp
+++ Section16/Section16Challenge/Section16Challenge/Account_Util.cpp
@@ -13,7 +13,7 @@ Account_Util::~Account_Util()
 void Account_Util::display(const std::vector<Account*>& accounts) {
 	std::cout << "\n=== Accounts ==========================================" << std::endl;
 	for (const auto& acc : accounts)
-		std::cout << acc << std::endl;
+		std::cout << *acc << std::endl;
 }
 
 // Deposits supplied amount to each Account object in the vector
@@ -21,9 +21,9 @@ void Account_Util::deposit(std::vector<Account*>& accounts, double amount) {
 	std::cout << "\n=== Depositing to Accounts =================================" << std::endl;
 	for (auto& acc : accounts) {
 		if (acc->deposit(amount))
-			std::cout << "Deposited " << amount << " to " << acc << std::endl;
+			std::cout << "Deposited " << amount << " to " << *acc << std::endl;
 		else
-			std::cout << "Failed Deposit of " << amount << " to " << acc << std::endl;
+			std::cout << "Failed Deposit of " << amount << " to " << *acc << std::endl;
 	}
 }
 
@@ -32,8 +32,8 @@ void Account_Util::withdraw(std::vector<Account*>& accounts, double amount) {
 	std::cout << "\n=== Withdrawing from Accounts ==============================" << std::endl;
 	for (auto& acc : accounts) {
 		if (acc->withdraw(amount))
-			std::cout << "Withdrew " << amount << " from " << acc << std::endl;
+			std::cout << "Withdrew " << amount << " from " << *acc << std::endl;
 		else
-			std::cout << "Failed Withdrawal of " << amount << " from " << acc << std::endl;
+			std::cout << "Failed Withdrawal of " << amount << " from " << *acc << std::endl;
 	}
 }
