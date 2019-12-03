50cd217e4a32577d7afef4c42d421e65b11198bf
tslothorst <tslothorst@gmail.com>
2019-12-03 10:20:35 +0100

    Implemented grading

--- Section19/Section19Challenge2/Section19Challenge2/main.cpp
+++ Section19/Section19Challenge2/Section19Challenge2/main.cpp
@@ -9,9 +9,31 @@ int main()
 	if (!respons_file) 
 	{
 		std::cerr << "Error reading from file \n";
+		return 1;
 	}
-	
 
+	std::cout << std::setw(15) << std::left << "Student Name" << std::setw(5) << std::right << "Score" << std::endl;
 
+	std::string AnswerKey{};
+	respons_file >> AnswerKey;
+	while (!respons_file.eof()) 
+	{
+		std::string StudentName{};
+		std::string StudentAnswers{};
+		int score{ 0 };
+		respons_file >> StudentName;
+		respons_file >> StudentAnswers;
+
+		for (size_t i = 0; i < StudentAnswers.length(); i++)
+		{
+			if (StudentAnswers[i] == AnswerKey[i]) 
+			{
+				++score;
+			}
+		}
+
+		std::cout << std::setw(15) << std::left << StudentName << std::setw(5) << std::right <<score << std::endl;
+
+	}
 	return 0;
 }
