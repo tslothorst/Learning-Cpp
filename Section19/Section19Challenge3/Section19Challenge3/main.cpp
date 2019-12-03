22a979efec9ccce3d0c0ee4cd7f00b5345ec3bca
tslothorst <tslothorst@gmail.com>
2019-12-03 22:45:02 +0100

    started main.cpp

--- Section19/Section19Challenge3/Section19Challenge3/main.cpp
+++ Section19/Section19Challenge3/Section19Challenge3/main.cpp
@@ -4,5 +4,15 @@
 
 int main() 
 {
+	std::ifstream SourceText{ "romeoandjuliet.txt" };
+	if (!SourceText) 
+	{
+		std::cerr << "Error reading from file \n";
+	}
+
+	std::string Keyword{};
+	std::cout << "Enter word to search for: ";
+	std::cin >> Keyword;
+
 	return 0;
 }
