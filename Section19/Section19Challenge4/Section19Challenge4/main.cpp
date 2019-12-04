65b04cd507d5e9f0421c3816edbb7cda0d5e0557
tslothorst <tslothorst@gmail.com>
2019-12-04 22:59:52 +0100

    Added copy function

--- Section19/Section19Challenge4/Section19Challenge4/main.cpp
+++ Section19/Section19Challenge4/Section19Challenge4/main.cpp
@@ -2,8 +2,33 @@
 #include<iostream>
 #include<fstream>
 #include<iomanip>
+#include<string>
 
 int main()
 {
+	std::ifstream InputFile{ "romeoandjuliet.txt" };
+
+	if(!InputFile)
+	{
+		std::cerr << "Error reading from input file\n";
+		return 1;
+	}
+
+	std::ofstream OutputFile{ "romeoandjuliet_linenumbers.txt" };
+
+	if(!OutputFile)
+	{
+		std::cerr << "Error creating output file\n";
+		return 1;
+	}
+
+	while(!InputFile.eof())
+	{
+		int Iteration{ 1 };
+		std::string Temp{};
+		std::getline(InputFile, Temp);
+		OutputFile << std::setw(8) << std::left << Iteration << std::left << Temp << std::endl;
+	}
+
 	return 0;
 }
