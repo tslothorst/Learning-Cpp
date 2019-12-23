a986442b66e528492f1a3076f39e29171f8ce5ca
tslothorst <tslothorst@gmail.com>
2019-12-23 20:34:34 +0100

    Added Section 20 Challenge 4

---
+++ Section20/Section20Challenge4/Section20Challenge4/main.cpp
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <cctype>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+bool is_palindrome(const std::string& s)
+{
+    std::deque<char> d1;
+    std::deque<char> d2;
+    for (auto letter : s)
+    {
+        if (isalpha(letter))
+        {
+            d1.push_back(std::tolower(letter));
+            d2.push_front(std::tolower(letter));
+        }
+    }
+
+    if (d1 == d2)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
+
+    std::cout << std::boolalpha;
+    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+    for (const auto& s : test_strings) {
+        std::cout << std::setw(8) << std::left << is_palindrome(s) << s << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
